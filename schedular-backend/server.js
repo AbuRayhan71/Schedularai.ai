679e221fc41c4015493d624360ab2f9ef6363739
MD ABU RAYHAN <aburayhanisar71@gmail.com>
2024-05-15 03:48:22 +1000

    done
    new

--- schedular-backend/server.js
+++ schedular-backend/server.js
@@ -14,6 +14,7 @@ mongoose.connect('mongodb://localhost:27017/scheduler')
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB:', err));
 
+  
 // Routes
 const schedulesRoute = require('./routes/schedules');
 app.use('/schedules', schedulesRoute);
